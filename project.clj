916413217ff22e1e3645734c6a72a45a33a703c4
Ikuru Kyogoku <kanumaiku@gmail.com>
2019-07-28 20:30:54 -0700

    Publish snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject umlaut-mysql-ddl "0.1.0-SNAPSHOT"
+(defproject xcoo/umlaut-mysql-ddl "0.1.0-SNAPSHOT"
   :dependencies [[umlaut "0.6.2"]
                  [weavejester/dependency "0.2.1"]]
   :profiles {:dev {:source-paths ["src"]
