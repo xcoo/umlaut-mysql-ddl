afee238069e5d22ccb77bba27b77d5f44d8ef046
Ikuru Kyogoku <kanumaiku@gmail.com>
2019-08-08 07:49:00 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xcoo/umlaut-mysql-ddl "0.1.0-SNAPSHOT"
+(defproject xcoo/umlaut-mysql-ddl "0.1.1-SNAPSHOT"
   :dependencies [[umlaut "0.6.2"]
                  [weavejester/dependency "0.2.1"]]
   :profiles {:dev {:source-paths ["src"]
